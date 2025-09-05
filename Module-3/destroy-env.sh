4dc7e5e7f5232983606d3117e4afe5966419cba4
phatpitou <59967188+phatpitou@users.noreply.github.com>
2025-09-06 00:54:51 +0700

    update destroy

--- Module-3/destroy-env.sh
+++ Module-3/destroy-env.sh
@@ -8,8 +8,7 @@
 echo "Beginning destroy script for module-03 assessment..."
 
 # Collect Instance IDs
-# https://stackoverflow.com/questions/31744316/aws-cli-filter-or-logic
-INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' --filter "Name=instance-state-name,Values=running,pending" "Name=tag:Name,Values=module3-tag")
+INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' --filter "Name=instance-state-name,Values=running,pending" "Name=tag:Name,Values=${7}")
 echo "List of INSTANCEIDS to deregister..."
 if [ "$INSTANCEIDS" == "" ];
   then
@@ -19,15 +18,12 @@ else
 fi 
 
 echo "Finding TARGETARN..."
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-target-groups.html
-TARGETARN=$(aws elbv2 describe-target-groups --names "your-initials-tg" --query 'TargetGroups[0].TargetGroupArn' --output text 2>/dev/null)
+TARGETARN=$(aws elbv2 describe-target-groups --names "${8}" --query 'TargetGroups[0].TargetGroupArn' --output text 2>/dev/null)
 echo $TARGETARN
 
 if [ "$INSTANCEIDS" != "" ]
   then
     echo '$INSTANCEIDS to be deregistered with the target group...'
-    # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/register-targets.html
-    # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
     INSTANCEIDSARRAY=($INSTANCEIDS)
     for INSTANCEID in ${INSTANCEIDSARRAY[@]};
       do
@@ -40,7 +36,6 @@ if [ "$INSTANCEIDS" != "" ]
     echo 'There are no running or pending values in $INSTANCEIDS to wait for...'
 fi 
 
-#https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/wait/instance-running.html
 echo "Now terminating the detached INSTANCEIDS..."
 if [ "$INSTANCEIDS" != "" ]
   then
@@ -53,13 +48,9 @@ if [ "$INSTANCEIDS" != "" ]
 fi 
 
 echo "Looking up ELB ARN..."
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-load-balancers.html
-ELBARN=$(aws elbv2 describe-load-balancers --names "your-initials-elb" --query 'LoadBalancers[0].LoadBalancerArn' --output text 2>/dev/null)
+ELBARN=$(aws elbv2 describe-load-balancers --names "${9}" --query 'LoadBalancers[0].LoadBalancerArn' --output text 2>/dev/null)
 echo $ELBARN
 
-# Collect ListenerARN
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-listeners.html
- # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
 if [ "$ELBARN" != "" ] && [ "$ELBARN" != "None" ]; then
     LISTENERARNS=$(aws elbv2 describe-listeners --load-balancer-arn $ELBARN --query='Listeners[*].ListenerArn' --output text)
     for LISTENERARN in $LISTENERARNS;
@@ -72,17 +63,14 @@ else
     echo "No ELB or listeners found to delete."
 fi
 
-
 if [ "$TARGETARN" = "" ] || [ "$TARGETARN" = "None" ];
   then  
   echo "No Target Groups to delete..."
 else
   echo "Deleting target group $TARGETARN..."
-  # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
   TARGETARNSARRAY=($TARGETARN)
     for TGARN in ${TARGETARNSARRAY[@]};
       do
-        # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/delete-target-group.html
         aws elbv2 delete-target-group --target-group-arn $TGARN
       done
 fi
@@ -92,11 +80,9 @@ if [ "$ELBARN" = "" ] || [ "$ELBARN" = "None" ];
   echo "No ELBs to delete..."
 else
   echo "Issuing Command to delete Load Balancer.."
-  # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/delete-load-balancer.html
   aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
   echo "Load Balancer delete command has been issued..."
 
   echo "Waiting for ELB to be deleted..."
-  # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/wait/load-balancers-deleted.html#examples
   aws elbv2 wait load-balancers-deleted --load-balancer-arns $ELBARN
 fi
